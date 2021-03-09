cc64516e1fa5ce502208c082aca5f4e75e8603dd
Javiera Zamorano <jzamorano@confluence.com>
2021-03-09 18:11:48 +0000

    Changes to button clicking logic

--- src/Calculator.js
+++ src/Calculator.js
@@ -16,11 +16,10 @@ function Calculator() {
   }
   function Evaluate(sum) {
     //split string up by space delimiter
-    const splitSum = sum.split(/[ ]/);
+    const splitSum = sum.split(/(?=[-+/\*])|(?<=[-+/\*])/g);
 
     let i;
     for (i = 0; i < splitSum.length; i++) {
-      
       const leadChar = splitSum[i].slice(0, 1);
 
       if (leadChar == 0) {
@@ -28,49 +27,64 @@ function Calculator() {
       }
     }
 
-  
     return splitSum;
   }
 
   function evalReplacement(calcInput) {
-
     const toBeEvaled = Evaluate(calcInput);
-
-    let runningCalc = null;
+debugger;
+    let runningCalc = 0;
     let isOperator = false;
     let operator;
 
-    toBeEvaled.forEach(element => {
-      if (isOperator) {
+    toBeEvaled.forEach((element,index) => {
+      if (isOperator && (parseFloat(element) - parseFloat(element) === 0)) {
         isOperator = false;
-        runningCalc = operator(runningCalc, parseFloat(element))
+        runningCalc = operator(runningCalc, parseFloat(element));
       }
+     /* else if (toBeEvaled[index-1]==="+" && toBeEvaled[index]==="-" || toBeEvaled[index-1]==="-" && toBeEvaled[index]==="+") {
+        isOperator = true;
+        operator = (current, newValue) => current - newValue;
+      }*/
 
       if (element === "+") {
         isOperator = true;
         operator = (current, newValue) => current + newValue;
-      }
-
-      else if (element === "-") {
+      } else if (element === "-") {
         isOperator = true;
         operator = (current, newValue) => current - newValue;
-      }
-
-      else if (element === "/") {
+      } else if (element === "/") {
         isOperator = true;
         operator = (current, newValue) => current / newValue;
-      }
-
-      else if (element === "*") {
+      } else if (element === "*") {
         isOperator = true;
         operator = (current, newValue) => current * newValue;
       }
 
-      if (runningCalc === null) {
-        runningCalc = parseFloat(element)
+      if (runningCalc === 0) {
+        runningCalc = parseFloat(element);
       }
     });
-    return runningCalc
+    return runningCalc;
+  }
+
+  function isCharacterAllowed(pressedChar, prevChar) {
+    const plusArray = ["+","-","*","/","(",")","0","1","2","3","4","5","6","7","8","9",undefined];
+    const minusArray = ["+","-","*","/","(",")","0","1","2","3","4","5","6","7","8","9",undefined];
+    const timesDivideArray = [")","0","1","2","3","4","5","6","7","8","9",undefined];
+debugger;
+    switch(pressedChar) {
+      case "+":
+        return plusArray.includes(prevChar);
+      case "-":
+        return minusArray.includes(prevChar);
+      case "*":
+      case "/":
+        return timesDivideArray.includes(prevChar);
+      default:
+        return true;
+    
+    }
   }
 
   return (
@@ -85,18 +99,18 @@ function Calculator() {
           <div onClick={() => setRunningTot("0")}>0</div>
         </div>
         <div className="coltwo">
-          <div onClick={() => setRunningTot(" ( ")}>(</div>
+          <div onClick={() => setRunningTot("(")}>(</div>
           <div onClick={() => setRunningTot("8")}>8</div>
           <div onClick={() => setRunningTot("5")}>5</div>
           <div onClick={() => setRunningTot("2")}>2</div>
           <div onClick={() => setRunningTot(".")}>.</div>
         </div>
         <div className="colthree">
-          <div onClick={() => setRunningTot(" ) ")}>)</div>
+          <div onClick={() => setRunningTot(")")}>)</div>
           <div onClick={() => setRunningTot("9")}>9</div>
           <div onClick={() => setRunningTot("6")}>6</div>
           <div onClick={() => setRunningTot("3")}>3</div>
-          <div onClick={() => setRunningTot(" ** ")}>EXP</div>
+          <div onClick={() => setRunningTot("**")}>EXP</div>
         </div>
         <div className="colfour">
           <div
@@ -111,8 +125,21 @@ function Calculator() {
           >
             DEL
           </div>
-          <div onClick={() => setRunningTot(" * ")}>x</div>
-          <div onClick={() => setRunningTot(" + ")}>+</div>
+          <div onClick={() => setRunningTot("*")}>x</div>
+          <div
+            onClick={() => {
+              if (
+                isCharacterAllowed(
+                  "+",
+                  CalcyState.runningtot[CalcyState.runningtot.length - 1]
+                )
+              ) {
+                setRunningTot("+");
+              }
+            }}
+          >
+            +
+          </div>
           <div
             onClick={() =>
               setCalcyState(function (oldCalcState) {
@@ -139,8 +166,8 @@ function Calculator() {
           >
             AC
           </div>
-          <div onClick={() => setRunningTot(" / ")}>/</div>
-          <div onClick={() => setRunningTot(" - ")}>-</div>
+          <div onClick={() => setRunningTot("/")}>/</div>
+          <div onClick={() => setRunningTot("-")}>-</div>
           <div
             onClick={() =>
               setCalcyState(function (oldCalcState) {
@@ -160,4 +187,3 @@ function Calculator() {
 }
 
 export default Calculator;
-
